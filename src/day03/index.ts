cac76a4656435759c65c840c2ed9a7b904381230
Jacob Dunn <jacob.dunn@aviture.us.com>
2024-12-03 09:54:17 -0700

    Day 3

---
+++ src/day03/index.ts
@@ -0,0 +1,35 @@
+import run from "aocrunner";
+
+const mul = (sum: number, matches: RegExpExecArray) =>
+  sum + parseInt(matches[1]) * parseInt(matches[2]);
+
+const part1 = (rawInput: string) =>
+  [...rawInput.matchAll(/mul\((\d+),(\d+)\)/gs)].reduce(mul, 0);
+
+const part2 = (rawInput: string) =>
+  [
+    ...rawInput.matchAll(/(?<!don't\(\)(?:(?!do\(\)).)*)mul\((\d+),(\d+)\)/gs),
+  ].reduce(mul, 0);
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
+        expected: 161,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+        expected: 48,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
