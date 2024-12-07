604ce6ae5c1c803569e508235a9a44a655268e4c
Jacob Dunn <jacob.dunn@aviture.us.com>
2024-12-06 23:08:48 -0700

    Day 7 Cleanup

--- src/day07/index.ts
+++ src/day07/index.ts
@@ -1,25 +1,22 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) =>
-  rawInput.split("\n").map((line) => {
+const parseInput = (
+  rawInput: string,
+  addCalculation: (options: number[], number: number) => number[],
+) => {
+  const equations = rawInput.split("\n").map((line) => {
     const [solution, numbers] = line.split(": ");
     return {
       solution: parseInt(solution),
       numbers: numbers.split(" ").map((part) => parseInt(part)),
     };
   });
-
-const part1 = (rawInput: string) => {
-  const equations = parseInput(rawInput);
   return equations.reduce((sum, equation) => {
     const options = equation.numbers.reduce<number[]>((current, number) => {
       if (!current.length) {
         return [number];
       }
-      return [
-        ...current.map((option) => option * number),
-        ...current.map((option) => option + number),
-      ];
+      return addCalculation(current, number);
     }, []);
     if (options.includes(equation.solution)) {
       return sum + equation.solution;
@@ -28,24 +25,19 @@ const part1 = (rawInput: string) => {
   }, 0);
 };
 
+const part1 = (rawInput: string) => {
+  return parseInput(rawInput, (options, number) => [
+    ...options.map((option) => option * number),
+    ...options.map((option) => option + number),
+  ]);
+};
+
 const part2 = (rawInput: string) => {
-  const equations = parseInput(rawInput);
-  return equations.reduce((sum, equation) => {
-    const options = equation.numbers.reduce<number[]>((current, number) => {
-      if (!current.length) {
-        return [number];
-      }
-      return [
-        ...current.map((option) => option * number),
-        ...current.map((option) => option + number),
-        ...current.map((option) => parseInt(`${option}${number}`)),
-      ];
-    }, []);
-    if (options.includes(equation.solution)) {
-      return sum + equation.solution;
-    }
-    return sum;
-  }, 0);
+  return parseInput(rawInput, (options, number) => [
+    ...options.map((option) => option * number),
+    ...options.map((option) => option + number),
+    ...options.map((option) => parseInt(`${option}${number}`)),
+  ]);
 };
 
 const input = `190: 10 19
